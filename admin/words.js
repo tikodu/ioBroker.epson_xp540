6d77f0e250ba8024343366a03fb2b4a28229ef83
Till <till@dusoft.de>
2021-01-16 16:23:14 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'epson_xp540 adapter settings': {
+		en: 'Adapter settings for epson_xp540',
+		de: 'Adaptereinstellungen für epson_xp540',
+		ru: 'Настройки адаптера для epson_xp540',
+		pt: 'Configurações do adaptador para epson_xp540',
+		nl: 'Adapterinstellingen voor epson_xp540',
+		fr: "Paramètres d'adaptateur pour epson_xp540",
+		it: "Impostazioni dell'adattatore per epson_xp540",
+		es: 'Ajustes del adaptador para epson_xp540',
+		pl: 'Ustawienia adaptera dla epson_xp540',
+		'zh-cn': 'epson_xp540的适配器设置',
+	},
+	option1: {
+		en: 'option1',
+		de: 'Option 1',
+		ru: 'Опция 1',
+		pt: 'Opção 1',
+		nl: 'Optie 1',
+		fr: 'Option 1',
+		it: 'opzione 1',
+		es: 'Opción 1',
+		pl: 'opcja 1',
+		'zh-cn': '选项1',
+	},
+	option2: {
+		en: 'option2',
+		de: 'Option 2',
+		ru: 'option2',
+		pt: 'opção 2',
+		nl: 'Optie 2',
+		fr: 'Option 2',
+		it: 'opzione 2',
+		es: 'opcion 2',
+		pl: 'Opcja 2',
+		'zh-cn': '选项2',
+	},
+};
